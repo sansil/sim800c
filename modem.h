b1a3a3688198755559866814a24ea16ea6d8d55e
sansil <sansil32@gmail.com>
2019-03-15 14:39:46 -0300

    adding modem library

--- modem.h
+++ modem.h
@@ -31,4 +31,73 @@ void rxBuffer::clear()
   rxBuffer::ind = 0;
 }
 
+typedef enum error_modem_t
+{
+  ERROR_MDM = 0,
+  BUSY_MDM,
+  OK_MDM
+} error_modem_t;
+
+typedef enum request_t
+{
+  modem_begin = 0,
+  modem_getcclk,
+  modem_setcclk,
+  modem_getimei,
+  modem_sendgprs,
+  modem_getrssi,
+  modem_off,
+  modem_download_ftp
+} request_t; // request a pedirle desde el exterior
+
+typedef enum modem_state_t
+{
+  inicio,
+  registro_red,
+  imei,
+  fecha_hora,
+  hora_red,
+  guardar_busqueda_hora_red,
+  rm_eco,
+  rssi,
+  apagar_mdm,
+  software_reset,
+  http_config_1,
+  http_config_2,
+  http_config_3,
+  http_config_4,
+  http_send_msg_1,
+  http_send_msg_2,
+  http_send_msg_3,
+  http_send_msg_4,
+  http_send_msg_5,
+  http_send_msg_6,
+  http_send_msg_7,
+  http_send_msg_8,
+  http_send_msg_9,
+  config_ftp_1,
+  config_ftp_2,
+  config_ftp_3,
+  config_ftp_4,
+  config_ftp_5,
+  download_ftp_1,
+  download_ftp_2,
+  download_ftp_3,
+  download_ftp_4,
+  download_ftp_5,
+  no_op,
+  error_mdm,
+  esperando_respuesta,
+  procesar_respuesta,
+  procesar_mensaje
+} modem_state_t; // request a pedirle desde el exterior
+
+typedef struct s_state_t
+{
+  modem_state_t estado_actual;
+  modem_state_t estado_proximo;
+  modem_state_t estado_anterior;
+  char resp_esperada[30];
+} s_state_t;
+
 #endif
